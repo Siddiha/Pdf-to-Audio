5f5a9378814c14988215126c95facfc36dc4248d
Siddiha <fathimasiddika62@gmail.com>
2025-04-16 08:47:36 +0530

    dfg

--- Audio.py
+++ Audio.py
@@ -2,14 +2,13 @@ import pyttsx3
 import PyPDF2
 from tkinter.filedialog import *
 
-
 book = askopenfilename()
-pdfreader = PyPDF2.PdfFileReader(book)
-pages = pdfreader.numPages
+pdfreader = PyPDF2.PdfReader(book)  # Updated to PdfReader
+pages = len(pdfreader.pages)  # Updated to get the number of pages
 
 for num in range(0, pages):
-    page = pdfreader.getPage(num)
-    text = page.extractText()
+    page = pdfreader.pages[num]  # Updated to access pages
+    text = page.extract_text()  # Updated to extract text
     player = pyttsx3.init()
     player.say(text)
-    player.runAndWait
+    player.runAndWait()
